ff987b8825d7566852c7c80285abb2057d2f8d4d
AbrahamGarcia22 <42215202+AbrahamGarcia22@users.noreply.github.com>
2021-03-29 20:15:04 -0600

    Add files via upload

--- UserProfile.js
+++ UserProfile.js
@@ -1,16 +1,36 @@
-import React, {useState} from 'react';
-import { StyleSheet, View, Image, TextInput, Button } from 'react-native';
+import React, {useReducer, useState} from 'react';
+import { StyleSheet, View, Image, TextInput, Button,FlatList, useColorScheme } from 'react-native';
+import {Feed} from './Feed';
+
 
 export const UserProfile = (props) => {
   
   const data = props.data; 
+  const _lista = props.lista;
+  const user=props.userD;
 
   const [text, onChangeText] = React.useState("");
   
   const [isFocus, setIsFocus] = useState(false);
-
   const onFocus = () =>{
-    setIsFocus(!isFocus)
+      setIsFocus(!isFocus);
+  }
+  function pushData()
+  {
+    if(text==""){
+      alert("Post vacio");
+
+    }else{
+    _lista.push({
+      "_id": _lista.length+1,
+      "name": user.name,
+      "image": user.image,
+      "description": text,
+      "like": true,
+    },)
+    onChangeText("");
+  }
+
   }
   
   return(
@@ -22,7 +42,7 @@ export const UserProfile = (props) => {
       />
       <TextInput 
         style={styles.textInput}
-        placeholder="What's your dog doing?"
+        placeholder="What's your doggo doing?"
         clearTextOnFocus={true}
         multiline
         numberOfLines={2}
@@ -31,12 +51,23 @@ export const UserProfile = (props) => {
         onChangeText={onChangeText}
         value={text}
         onFocus={() => onFocus()}
+        onBlur={()=>onFocus()}
+        on
       />
     </View>
     <View style={styles.button}>
-      { isFocus ? <Button title="Post" onPress={() => Alert.alert('Simple Button pressed')} />  
-                : null }
+      { isFocus ? <Button title="POST" onPress={()=>{pushData()}} />  
+                :null}
     </View>
+
+    <FlatList 
+      keyExtractor={ (item) => String(item['_id']) }
+      data = {_lista}
+      renderItem = { ({item}) =>(
+        <Feed data={item}/>
+      )}
+    />
+
     </>
   );
 }
@@ -49,7 +80,7 @@ const styles = StyleSheet.create({
     paddingBottom: 15
   },
   textInput:{
-    fontFamily: 'sans-serif-light',
+    fontFamily: 'Georgia',
     fontSize: 15,
     width: 330,
     color: 'black', 
